c4421e86883bad220bf4373ba607764679e3b303
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-08-11 14:29:50 -0400

    Add sinatra UI

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,3 +1,15 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+
+get '/caesar_cipher' do
+	word = params['word']
+	num = params['num'].to_i
+	color = word == "" ? 'white' : 'green'
+	cipherize = cipher(word, num)
+	erb :index, :locals => {:word => word, :num => num, :color => color, :cipherize => cipherize}
+end
+
 def cipher(word, num)
 alphabet = ('a'..'z').to_a.concat(('A'..'Z').to_a)
 new_word = ""
@@ -16,4 +28,4 @@ end
 return new_word
 end
 
-puts cipher("Apples? and Oranges!!!!!!", 5)
+#puts cipher("Apples? and Oranges!!!!!!", 5)
